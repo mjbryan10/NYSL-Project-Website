dd8a5957ea5313116557ed4560f741e87f2052f7
mjbryan <mjbyran10@gmail.com>
2020-01-17 13:06:29 +0100

    Footer and styles added + some corrections

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 
 
-let locations = document.querySelectorAll(".address-location");
+
 function clickTest(){
     console.log('Clicked!');
     console.log(this.getAttribute('data-url'));
 }
 
 function mapActivate(){
-    // let locations = document.querySelectorAll(".address-location");
     let map = document.querySelector('#google_map');
     let activated = document.querySelector('.address-location.active');
     activated.classList.toggle('active');
@@ -17,8 +16,8 @@ function mapActivate(){
 
 }
 window.onload = function () {
-    // document.querySelector('#google_map').setAttribute("src", document.querySelector('.address-location.active').getAttribute('data-url'));
-    for (i=0; i<locations.length; i++){
+    let locations = document.querySelectorAll(".address-location");
+    for (let i=0; i<locations.length; i++){
         locations[i].addEventListener("click", mapActivate);
     }
 }
