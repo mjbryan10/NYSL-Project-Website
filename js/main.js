7e725db94aa91aeba0db72f58ea26e3b12461359
mjbryan <mjbyran10@gmail.com>
2020-01-20 09:44:21 +0100

    Added grey out to uniform options

--- js/main.js
+++ js/main.js
@@ -45,8 +45,22 @@ scrollBtn.addEventListener("click", function(){ //Caller
     smoothScroll('h1', 1000);
 });
 //------
-
-
+// --- UNIFORM GREYOUT ---
+let greyOut = function(trigger, greyTar, unCheck){
+    for (grey of greyTar){ //toggles grey class
+        if ((trigger.checked && !grey.classList.contains('greyed-out')) || (!trigger.checked && grey.classList.contains('greyed-out'))){
+            grey.classList.toggle('greyed-out');
+        }
+    }
+    if (trigger.checked){ //-- clears the radios when checked.
+        for (radio of unCheck){
+            if (radio.checked){
+                radio.checked = false;
+            }    
+        }
+    }    
+}
+//-----
 
 
 
@@ -54,10 +68,18 @@ scrollBtn.addEventListener("click", function(){ //Caller
 
 
 window.onload = function () {
+    //ADDRESS SWAPPER
     let locations = document.querySelectorAll(".address-location");
     for (let i=0; i<locations.length; i++){
         locations[i].addEventListener("click", mapActivate);
     }
+    //UNIFORM
+    let uniforms = document.querySelectorAll(".uniform-options");
+    let uniformCheck = document.querySelector('#uniform');
+    let unCheckUniform = document.querySelectorAll(".sizes-list input");
+    uniformCheck.addEventListener("change", function(){
+        greyOut(uniformCheck, uniforms, unCheckUniform);
+    });
 }
 
 let scrollVisible = function() { //makes scroll btn visible if page is longer than height px
